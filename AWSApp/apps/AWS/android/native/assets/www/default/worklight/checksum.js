960730729b22d8ce9a5dd8a923a46a18b9872f75
Philippe Oscar Sanoy <sanoyphilippe@gmail.com>
2016-01-13 11:18:16 +0800

    re-upload of workspace part2

---
+++ AWSApp/apps/AWS/android/native/assets/www/default/worklight/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"date":1452653079114,"machine":"MKT-001-218-15.awsys-i.com","checksum":4199150553};
+/* Date: Wed Jan 13 10:44:39 SGT 2016 */
---
+++ AWSApp/apps/AWS/android/native/src/com/AWS/GCMIntentService.java
@@ -0,0 +1,5 @@
+package com.AWS;
+
+public class GCMIntentService extends com.worklight.androidgap.push.GCMIntentService{
+	//Nothing to do here...
+}
